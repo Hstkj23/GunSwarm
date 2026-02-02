af46ef5dd13013146fc517779ed52c28672f3d8a
Hase <86565570+Hstkj23@users.noreply.github.com>
2026-02-02 16:31:23 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/gunswarm/main.go
@@ -0,0 +1,23 @@
+// cmd/gunswarm/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "gunswarm/internal/gunswarm"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := gunswarm.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
